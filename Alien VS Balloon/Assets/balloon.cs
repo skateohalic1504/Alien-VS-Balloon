b17128cd44a9d581ae8b5729f1c3a0505cd7b36c
skateohalic1504 <jeffreyortizsantana1504@gmail.com>
2020-11-05 00:23:55 -0400

    globos png, loop de globos

---
+++ Alien VS Balloon/Assets/balloon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class balloon : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, speed);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < screenBounds.y * -2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Alien VS Balloon/Assets/constantMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class constantMove : MonoBehaviour
+{
+
+    gameManager GameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
+        GameManager = gameController.GetComponent<gameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(GameManager.moveVector * GameManager.moveSpeed * Time.deltaTime);
+    }
+}
